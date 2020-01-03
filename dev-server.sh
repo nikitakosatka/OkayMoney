cd107c93cc8f8e77f0c31ef1627d1b5183020ea1
javabird25 <shockck84@gmail.com>
2020-01-03 18:16:39 +0500

    Прокачал скрипт для запуска сервера

--- dev-server.sh
+++ dev-server.sh
@@ -1,2 +1,21 @@
-manage.py runserver &
+#!/bin/sh
+
+set -e
+
+die() {
+    echo "[31m$*[0m"
+    exit 1
+}
+
+test -e manage.py || die 'Скрипт должен быть запущен в корне проекта (где лежит manage.py)'
+which sass >/dev/null || die 'SASS не установлен. (https://sass-lang.com/install)'
+
+# Прибить django и sass при выходе
+trap 'kill $(jobs -p)' EXIT
+
+source venv/bin/activate
+./manage.py runserver &
 sass --watch okaymoneyapp/sass:okaymoneyapp/static/okaymoneyapp/css &
+while true; do
+    true  # Бесконечный цикл
+done
