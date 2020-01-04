1509d90ed83e732e78c5b765f0a5e2015a4de885
javabird25 <shockck84@gmail.com>
2020-01-04 20:37:05 +0500

    Favicon

--- okaymoney/urls.py
+++ okaymoney/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django_registration.backends.one_step.views import RegistrationView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,5 +12,6 @@ urlpatterns = [
         name="django_registration_register",
     ),
     path("accounts/", include("django_registration.backends.one_step.urls")),
+    path("favicon.ico", RedirectView.as_view(url="static/okaymoneyapp/images/icon.png")),
     path("", include("okaymoneyapp.urls")),
 ]
